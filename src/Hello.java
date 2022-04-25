ccee6e57ee83b36a08de255ef7d1b0720c2d186e
120L020818 <120L020818@stu.hit.edu.cn>
2022-04-25 13:11:44 +0800

    test

--- src/Hello.java
+++ src/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
         System.out.println("我成功地使用了github!");
         System.out.println("为什么账号不同?");
         System.out.println("修正之后的账号");
+        System.out.println("好像没有成功");
     }
 }
